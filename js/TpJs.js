daaeb043ff773391e0afa6e114d2596f515119cf
unknown <juanvillan777@gmail.com>
2022-05-02 23:36:08 -0300

    js exercise

---
+++ js/TpJs.js
@@ -0,0 +1,19 @@
+var num1 = 9;
+//console.log (num1)//
+var num2 = 16;
+var num3 = 4;
+if (num1 > num2 && num1 > num3){
+  console.log (num1,"es el mas grande")
+}else if (num2 > num1 && num2 > num3){
+  console.log(num2,"es el mas grande")
+}else {
+  console.log(num3,"es el mas grande")
+};
+
+if (num1 < num2 && num1 < num3){
+  console.log (num1,"es el mas chico")
+}else if (num2 < num1 && num2 < num3){
+  console.log(num2,"es el mas chico")
+}else {
+  console.log(num3,"es el mas chico")
+};
